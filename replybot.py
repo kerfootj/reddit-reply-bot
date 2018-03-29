28a5e32a4c2709f10b4548daeae8d1af39aadb60
Joel Kerfoot <joelkerfoot@gmail.com>
2018-03-29 12:53:37 -0700

    Update replybot.py

--- replybot.py
+++ replybot.py
@@ -6,6 +6,7 @@
 import praw
 import time
 import sys
+import traceback
 import progressbar
 from fuzzywuzzy import fuzz
 
@@ -22,14 +23,19 @@ class bot:
 		self.sleeptime = sleeptime
 		self.debug = debug
 		self.numComments = 0
+		self.timeout = 1
 		self.reddit = None
 	
 	# Start the bot 
 	def start(self):
-		print('Logging in...')
-		self._login()
-		print('Logged in as ' + str(self.reddit.user.me()) + '\n')
-		return self._run()
+		try:
+			print('Logging in...')
+			self._login()
+			print('Logged in as ' + str(self.reddit.user.me()) + '\n')
+			self.timeout = 10
+			return self._run()
+		except Exception as e:
+			self._handle_exception(e)
 	
 	# Log in to the reddit account in praw.ini
 	def _login(self):
@@ -42,7 +48,7 @@ class bot:
 			if self.numComments != 0:
 				print('%d replies made' % self.numComments)
 				self.numComments = 0
-			self._sleep()
+			self._sleep(self.sleeptime, 'Sleeping')
 	
 	# Search the comments for a fuzzy match to a provided phrase
 	def _search_comments(self):
@@ -64,9 +70,16 @@ class bot:
 				# Check each comment for a fuzzy match to one of the quotes
 				for comment in submission.comments.list():
 					for i in range(0, len(self.quotes)):
-						if self.keywords[i].upper() in comment.body.upper():
-							if fuzz.ratio(self.quotes[i], comment.body) >= self.ratio:
-								usedQuotes.append([self.quotes[i], comment])
+						if comment != None and comment.author != None:
+							if comment.author.name != self.reddit.user.me():
+								if self.keywords[i].upper() in comment.body.upper():
+									if fuzz.ratio(self.quotes[i], comment.body) >= self.ratio:
+										usedQuotes.append([self.quotes[i], comment])
+							else:
+								print('Already replied to this thread')
+								break
+						else:
+							break
 				
 				# If a match was found try and reply to the comment
 				if 0 < len(usedQuotes) and len(usedQuotes) < len(self.quotes):
@@ -79,15 +92,12 @@ class bot:
 			if quote not in d:
 				# Reply to the last relevant comment in the thread 
 				comment = usedQuotes[len(usedQuotes)-1][1]
-				# Check so you don't reply to yourself
-				if comment.author.name != self.reddit.user.me():
-					print('Replying to a comment...\n')
-					comment.upvote()
-					reply = comment.reply(quote)
-					self._log_comment(comment, reply)
-					self.numComments += 1
-				else:
-					print('Already replied to this thread')
+				print('Replying to a comment...\n')
+				comment.upvote()
+				reply = comment.reply(quote)
+				self._log_comment(comment, reply)
+				self.numComments += 1
+				
 				return
 		print('All replies already taken...')
 		return
@@ -98,16 +108,40 @@ class bot:
 		f.write(str(comment) + '\t\t' + str(reply) + '\r\n')
 		f.close
 		
-	def _sleep(self):
+	def _sleep(self, st, message):
 		
 		cur = 0
-		t = 60 * self.sleeptime
+		t = 60 * st
 		
-		start_progress('Sleeping')
+		self._start_progress(message)
 		while(cur < t):
 			time.sleep(t / 10)
 			cur = cur + (t / 10)
-			progress(cur / t * 100)
-		end_progress()
+			self._progress(cur / t * 100)
+		self._end_progress()
 			
 	
+	def _start_progress(self, title):
+		global progress_x
+		sys.stdout.write(title + ': [' + '-'*40 + ']' + chr(8)*41)
+		sys.stdout.flush()
+		progress_x = 0
+			
+	def _progress(self, x):
+		global progress_x
+		x = int(x * 40 // 100)
+		sys.stdout.write('#' * (x - progress_x))
+		sys.stdout.flush()
+		progress_x = x
+		
+	def _end_progress(self):
+		sys.stdout.write('#' * (40 - progress_x) + ']\n\n')
+		sys.stdout.flush()
+		
+	def _handle_exception(self, e):
+		traceback.print_exc(limit=50)
+		self._sleep(self.timeout, 'Timed out, wait to retry')
+		self.timeout *= 2
+		self.start()
+		sys.exit(0)
+		 
